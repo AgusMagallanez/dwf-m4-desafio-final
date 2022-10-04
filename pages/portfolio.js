09be7a816ba372ab60528d52aa17fb7837563020
Agustina <agustinamagallanez.gmail.com>
2022-10-04 06:34:47 -0300

    commit 5 - start portfolio

---
+++ pages/portfolio.js
@@ -0,0 +1,57 @@
+function addCard(params = {}){
+    const template = document.querySelector("#portfolio-template");
+    const templatesContainer = document.querySelector(".portfolio-card-container");
+    
+    template.content.querySelector(".card-img").src = params.image;
+    template.content.querySelector(".card-title").textContent = params.title;
+    template.content.querySelector(".card-description").textContent = params.description;
+    template.content.querySelector(".card-url").href = params.url;
+
+    const clone = document.importNode(template.content , true);
+    templatesContainer.appendChild(clone);
+}
+
+function getImageData(id, data){
+    const  image = data.includes.Asset.find((i) =>{
+        return i.sys.id == id;
+    });
+    return image;
+}
+
+function getDataCMS(){
+    return fetch("https://cdn.contentful.com/spaces/bsclsxjn8ese/environments/master/entries?access_token=pRxuNwrd0MXHLQSDwQ3JiUC6WjIgzEPKB6HKictP9u0&content_type=portfolio")
+    
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const fields = data.items.map((item) =>{
+            const imageId = item.fields.imagen.sys.id;
+            const image = getImageData(imageId, data);
+            const imgLink = image.fields.file.url;
+
+            return {
+                image: imgLink,
+                title: item.fields.titulo,
+                description: item.fields.descripcion,
+                url: item.fields.url
+
+            };
+        });
+
+        return fields;
+    });
+}
+
+function main(){
+    getDataCMS().then(function(projects){
+        for(const p of projects){
+            addCard(p);
+        }
+    });
+
+
+}
+
+main();
